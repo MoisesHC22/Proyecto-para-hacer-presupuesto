e29c86b4fbbb66f331f2780ba8ebe3fdfcd7095b
MoisesHC <genesis2204gamerhc@gmail.com>
2024-02-29 16:23:54 -0600

    Tipos de validaciones

---
+++ Presupuesto/Models/TipoCuenta.cs
@@ -0,0 +1,40 @@
+﻿using Presupuesto.Validations;
+using System.ComponentModel.DataAnnotations;
+
+namespace Presupuesto.Models
+{
+    public class TipoCuenta
+    {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(maximumLength: 50, MinimumLength = 3,
+         ErrorMessage = "La longitud del campo {0} debe estar entre {2} y {1}  ")]
+        [Display(Name = "Nombre de el tipo de la cuenta")]
+
+
+        ///1. Probar los dos tipos de validaciones personalizadas.
+        [ValidarMayuscula]
+
+        public string Nombre { get; set; }
+        public int UduarioId { get; set; }
+        public int Orden { get; set; }
+
+        
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Nombre != null && Nombre.Length > 0)
+            {
+                var firtsLetter  = Nombre[0].ToString();
+                if (firtsLetter != firtsLetter.ToUpper())
+                {
+                    yield return new ValidationResult("La primera letra debe ser mayúscula",
+                    new[] {nameof(Nombre)} );
+                }
+            }
+        }
+
+    }
+}
---
+++ Presupuesto/Validations/ValidarMayusculaAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Presupuesto.Validations
+{
+    //Validación por atributo
+
+    public class ValidarMayusculaAttribute: ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if(value == null || string.IsNullOrEmpty(value.ToString()))
+            {
+                return ValidationResult.Success;
+            }
+            var firsLetter = value.ToString()[0].ToString();
+
+            if (firsLetter != firsLetter.ToUpper())
+            {
+                return new ValidationResult("La primera letra debe ser mayúscula");
+            }
+            return ValidationResult.Success;
+        }
+
+    }
+}
