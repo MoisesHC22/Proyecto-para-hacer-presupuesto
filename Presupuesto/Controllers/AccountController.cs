10252d4959c84a2a5de3ec161204906f5a9b0d7e
MoisesHC <genesis2204gamerhc@gmail.com>
2024-02-29 02:15:02 -0600

    Creación de un nuevo menú y login

---
+++ Presupuesto/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presupuesto.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SingIn()
+        {
+            return View();
+        }
+    }
+}
